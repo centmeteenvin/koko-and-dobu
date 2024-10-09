08e9dd1d9f2ebac9eda0c80007a59fd314e9e354
Vincent Verbergt <vincent.verbergt@gmail.com>
2024-10-09 17:34:19 +0200

    initial setup

--- koko_and_dobu_server/lib/src/generated/protocol.dart
+++ koko_and_dobu_server/lib/src/generated/protocol.dart
@@ -8,6 +8,7 @@
 // ignore_for_file: type_literal_in_constant_pattern
 // ignore_for_file: use_super_parameters
 
+// ignore: unnecessary_library_name
 library protocol; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
 import 'package:serverpod/serverpod.dart' as _i1;
@@ -22,9 +23,7 @@ class Protocol extends _i1.SerializationManagerServer {
 
   static final Protocol _instance = Protocol._();
 
-  static final List<_i2.TableDefinition> targetTableDefinitions = [
-    ..._i2.Protocol.targetTableDefinitions
-  ];
+  static final List<_i2.TableDefinition> targetTableDefinitions = [..._i2.Protocol.targetTableDefinitions];
 
   @override
   T deserialize<T>(
@@ -82,8 +81,7 @@ class Protocol extends _i1.SerializationManagerServer {
   }
 
   @override
-  List<_i2.TableDefinition> getTargetTableDefinitions() =>
-      targetTableDefinitions;
+  List<_i2.TableDefinition> getTargetTableDefinitions() => targetTableDefinitions;
 
   @override
   String getModuleName() => 'koko_and_dobu';
